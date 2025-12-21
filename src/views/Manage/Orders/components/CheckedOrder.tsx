acd16e2f2d7d1df7e2a05829dee15351f20d525b
ngocanh <ngocanh.pham19042001@gmail.com>
2025-12-21 11:51:22 +0700

    wooden-fe

---
+++ src/views/Manage/Orders/components/CheckedOrder.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+
+interface CheckedOrderProps{
+
+}
+
+const CheckedOrder = (props: CheckedOrderProps) => {
+    const { } = props;
+    return(
+        <Box>
+
+        </Box>
+    )
+}
+
+export default CheckedOrder;
