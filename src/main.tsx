f6624c4ffbc8ff81a6d6998e99b90b61d7a51a51
anhpn <ngocanh.pham19042001@gmail.com>
2026-02-09 16:55:15 +0700

    wooden-1

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClickToComponent editor='cursor' />
-    <BrowserRouter>
+    <BrowserRouter basename='/moc'>
       <App />
     </BrowserRouter>
   </StrictMode>,
